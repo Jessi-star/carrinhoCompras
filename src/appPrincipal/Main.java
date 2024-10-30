e07fa6b871c39688761825752a49689d35000632
Jessica Rodrigues <jessicarodrigues.enf1@gmail.com>
2024-10-30 16:24:50 -0300

    feat/add-construtores-e-metodos-get-set

--- src/appPrincipal/Main.java
+++ src/appPrincipal/Main.java
@@ -1,7 +1,30 @@
 package appPrincipal;
 
+import java.util.Scanner;
+import service.carrinhoConfig;
+
 public class Main {
     public static void main(String[] args) {
+        //CarrinhoConfig carrinhoConfig = new CarrinhoConfig();
+        Scanner scanner = new Scanner(System.in);
 
+        int opcoes = -1;
+        while (opcoes != 0){
+            System.out.println("*** Escolha uma opção para seu carrinho de compras: ");
+            System.out.println("1. Adicionar produto no carrinho");
+            System.out.println("2. Remover produtos do carrinho");
+            System.out.println("3. Atualizar a quantidade de produtos no carrinho");
+            System.out.println("4. Listar produtos no carrinho");
+            System.out.println("5. Calcular valor total do carrinho");
+            System.out.println("0. Sair do sistema");
+            opcoes = scanner.nextInt();
+
+        switch (opcoes) {
+            case 1:
+                adicionarProdutoCarrinho(carrinhoConfig, scanner);
+        }
+
+        }
+        scanner.close();
     }
 }
--- src/model/produtos.java
+++ src/model/produtos.java
@@ -7,6 +7,65 @@ public class produtos {
     private double valor;
     private int quantidade;
 
-    //preciso iniciar aqui os construtores, get e set (ver novamente nas aulas do Nelio como fazer)
+    public produtos() {
 
+    }
+    public produtos(int id, String nome, String categoria, double valor, int quantidade){
+        this.id = id;
+        this.nome = nome;
+        this.categoria = categoria;
+        this.valor = valor;
+        this.quantidade = quantidade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void  setNome() {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void  setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", nome= '" + nome + '\''+
+                ", categoria='" + categoria + '\'' +
+                ", valor=" + valor +
+                ", quantidade=" + quantidade +
+                '}';
+    }
 }
